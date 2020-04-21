138c669dfc10949e3638bb8d6f7e2519d35fb907
xuemengge <xuemengge@xingshulin.com>
2020-04-21 19:01:36 +0800

    fix: add comment on fetch-data-reducer

--- src/fetch-data-reducer.js
+++ src/fetch-data-reducer.js
@@ -38,25 +38,31 @@ const useDataApi = (initialUrl, initialData) => {
 
   useEffect(
     () => {
-      let didCancel = false
-      const fetchData = async () => {
-        dispatch({ type: "FETCH_INIT" })
-        try {
-          const result = await axios(url)
-          if (!didCancel) {
-            dispatch({ type: "FETCH_SUCCESS", payload: result.data })
-          }
-        } catch (error) {
-          if (!didCancel) {
-            dispatch({ type: "FETCH_FAILURE" })
-          }
-        }
-      }
-      fetchData()
-      return () => {didCancel = true}
-    },
-    [url]
+            let didCancel = false // 避免组件已经卸载但是组件的状态还是会被设置了
+            const fetchData = async () => {
+              dispatch({ type: "FETCH_INIT" })
+              try {
+                const result = await axios(url)
+                if (!didCancel) {
+                  dispatch({
+                    type: "FETCH_SUCCESS",
+                    payload: result.data,
+                  })
+                }
+              } catch (error) {
+                if (!didCancel) {
+                  dispatch({ type: "FETCH_FAILURE" })
+                }
+              }
+            }
+            fetchData()
+            return () => {
+              didCancel = true
+            }
+          },
+    [url] // dispatch可加可不加，因为dispatch在Prop改变的时候不会变
   )
+  /* 实际上数据获取并没有被终止（终止数据获取可以用Axios Cancellation来实现），但是已经挂载的组件不再执行状态转换。因为Axios Cancellation在我眼中不是最好的API，这个布尔值也能做阻止组件设置状态的工作。*/
 
   const doFetch = url => {
     setUrl(url)
